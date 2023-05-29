19bc80e022ec4a3f504e4501e4cb6d920e8a497d
shivambhagat79 <shivambhagat2279+github@gmail.com>
2023-05-29 11:24:57 +0530

    gallery page added

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent {
-
+  indices = Array.from({ length: 15 }, (_, i) => i + 1);
+  sources = this.indices.map((i) => `assets/img/gallery/img-${i}.jpg`);
 }
